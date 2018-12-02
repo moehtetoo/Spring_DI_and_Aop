9015aa41b30922b1f1025e9b59a4bfd3238f9645
moehtetoo <moehtat44444@gmail.com>
2018-12-02 22:27:25 +0630

    Add startUp method in BraveKnightTest

--- src/test/java/com/moehtet/BraveKnightTest.java
+++ src/test/java/com/moehtet/BraveKnightTest.java
@@ -4,14 +4,20 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BraveKnightTest {
-
+	Knight knight;
+	Quest mockQuest;
+	@Before
+	public void startUp() {
+		 mockQuest=mock(Quest.class);
+		 knight= new BraveKnight(mockQuest);
+	}
+	
 	@Test
 	public void knight_should_embark_on_quest() {
-		Quest mockQuest=mock(Quest.class);
-		Knight knight= new BraveKnight(mockQuest);
 		knight.embarkOnQuest();
 		verify(mockQuest,times(1)).embark();
 	}
